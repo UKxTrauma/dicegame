e03825144f4cbcc5a980b91663cfa1e2ce1d6c05
Brett Garrett <b.t.garrett@aol.co.uk>
2022-07-28 16:27:47 +0100

    main commit

--- app.js
+++ app.js
@@ -1,48 +1,130 @@
 let scoreCount = 0;
-const rollButt = document.getElementById('roll')
-const restartButt = document.getElementById('restart')
+    score1Count = 0;
+    score2Count = 0;
+    currentPlayer = 1;
+
+const rollButt = document.getElementById('roll');
+      restartButt = document.getElementById('restart');
+      newGame = document.getElementById('new');
 
 restartButt.addEventListener('click', ()=> {
     scoreCount = 0;
+    score1Count = 0;
+    score2Count = 0;
+    currentPlayer = 1;
     document.getElementById('condition').innerHTML = ``;
     document.getElementById('update').innerHTML = ``;
     document.getElementById('score').innerHTML = '> SCORE <';
+    document.getElementById('score1').innerHTML = 'PLAYER ONE<br>> SCORE <';
+    document.getElementById('score2').innerHTML = 'PLAYER TWO<br>> SCORE <';
     roll.style.visibility = 'visible';
     restart.style.visibility = 'hidden';
 });
 
-rollButt.addEventListener("click", ()=> {
-    let result = Math.floor(Math.random() * 6) + 1;
-    if(result>1){
-        scoreCount += result;
-        document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
-        document.getElementById('score').innerHTML = `${scoreCount}`;
+newGame.addEventListener('click', ()=> {
+    scoreCount = 0;
+    score1Count = 0;
+    score2Count = 0;
+    currentPlayer = 1;
+    document.getElementById('condition').innerHTML = ``;
+    document.getElementById('update').innerHTML = ``;
+    document.getElementById('score').innerHTML = '> SCORE <';
+    document.getElementById('score1').innerHTML = 'PLAYER ONE<br>> SCORE <';
+    document.getElementById('score2').innerHTML = 'PLAYER TWO<br>> SCORE <';
+    roll.style.visibility = 'visible';
+    restart.style.visibility = 'hidden';
+});
 
-        if(scoreCount>=20){
-            document.getElementById('update').innerHTML = `WINNER!!! YOU SCORED A TOTAL OF ${scoreCount}!`;
+
+////////////////////////////////////////////////////////////////////////
+rollButt.addEventListener("click", ()=> {
+    if(document.getElementById('score').style.display == 'block') {
+        let result = Math.floor(Math.random() * 6) + 1;
+        if(result>1){
+            scoreCount += result;
+            document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
+            document.getElementById('score').innerHTML = `${scoreCount}`;
+
+            if(scoreCount>=20){
+                document.getElementById('update').innerHTML = `WINNER!!! YOU SCORED A TOTAL OF ${scoreCount}!`;
+                scoreCount = 0;
+                rollButt.style.visibility = 'hidden';
+                restartButt.style.visibility = 'visible';
+            };
+        } else if(result==1){
             scoreCount = 0;
+            document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
+            document.getElementById('score').innerHTML = `${scoreCount}`;
+            document.getElementById('update').innerHTML = `YOU LOSE!!!`;
             rollButt.style.visibility = 'hidden';
             restartButt.style.visibility = 'visible';
-        };
-    } else if(result==1){
-        scoreCount = 0;
-        document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
-        document.getElementById('score').innerHTML = `${scoreCount}`;
-        document.getElementById('update').innerHTML = `YOU LOSE!!!`;
-        rollButt.style.visibility = 'hidden';
-        restartButt.style.visibility = 'visible';
+        } else {
+            document.getElementById('condition').innerHTML = `SOMETHING WENT WRONG, TRY AGAIN`;
+        }
     } else {
-        document.getElementById('condition').innerHTML = `SOMETHING WENT WRONG, TRY AGAIN`;
-    }
+        let result = Math.floor(Math.random() * 6) + 1;
+
+        if(result>1){
+
+            if(currentPlayer == 1){
+             score1Count += result;
+             document.getElementById('score1').innerHTML = `PLAYER ONE<br>${score1Count}`;
+             document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
+            } else {
+                score2Count += result;
+                document.getElementById('score2').innerHTML = `PLAYER TWO<br>${score2Count}`;
+                document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
+            };
+
+            if(score1Count>=20 || score2Count>=20){
+                if(currentPlayer == 1){
+                    document.getElementById('update').innerHTML = `PLAYER ONE WINS!!!`;
+                } else {
+                    document.getElementById('update').innerHTML = `PLAYER TWO WINS!!!`;
+                };
+                rollButt.style.visibility = 'hidden';
+                restartButt.style.visibility = 'visible';
+            };
+
+        } else if(result==1){
+            if(currentPlayer == 1){
+                score1Count = 0;
+                document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
+                document.getElementById('score1').innerHTML = `PLAYER ONE<br>${score1Count}`;
+                document.getElementById('update').innerHTML = `YOU LOSE, PLAYER TWO WINS!!!`;
+                rollButt.style.visibility = 'hidden';
+                restartButt.style.visibility = 'visible';
+            } else {
+                score2Count = 0;
+                document.getElementById('condition').innerHTML = `YOU ROLLED A ${result}!`;
+                document.getElementById('score2').innerHTML = `PLAYER TWO<br>${score2Count}`;
+                document.getElementById('update').innerHTML = `YOU LOSE, PLAYER ONE WINS!!!`;
+                rollButt.style.visibility = 'hidden';
+                restartButt.style.visibility = 'visible';
+            };
+        } else {
+            document.getElementById('condition').innerHTML = `SOMETHING WENT WRONG, TRY AGAIN`;
+        }
+    };
 });
+////////////////////////////////////////////////////////////////////////
+
+
+function holdButt () {
+    if(currentPlayer == 1){
+        currentPlayer = 2
+    } else {
+        currentPlayer = 1
+    }
+};
 
 function themeButt () {
     let change = document.getElementById("container");
-    let change2 = document.getElementById("game");
-    let change3 = document.getElementById("buttNaked");
-    let change4 = document.getElementById("info");
-    let change5 = document.getElementById("roll");
-    let change6 = document.getElementById("restart");
+        change2 = document.getElementById("game");
+        change3 = document.getElementById("buttNaked");
+        change4 = document.getElementById("info");
+        change5 = document.getElementById("roll");
+        change6 = document.getElementById("restart");
 
     change.classList.toggle("theme");
     change2.classList.toggle("theme");
@@ -50,4 +132,16 @@ function themeButt () {
     change4.classList.toggle("theme");
     change5.classList.toggle("theme");
     change6.classList.toggle("theme");
+};
+
+document.getElementById('score').style.display = 'block';
+function playerButt () {
+    if(document.getElementById('score').style.display === 'block'){
+        document.getElementById('score').style.display = 'none';
+        document.getElementById('twoPlayer').style.display = 'flex';
+    } else {
+        console.log('wlefvwlfg');
+        document.getElementById('twoPlayer').style.display = 'none';
+        document.getElementById('score').style.display = 'block';
+    }
 };
